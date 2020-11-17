2c1c6390963791703deba216db0e65dd91dbdf4d
Saranya619 <74487092+Saranya619@users.noreply.github.com>
2020-11-17 14:39:55 +0530

    uploaded my files

---
+++ GitExample/src/main/java/LinearSearch.java
@@ -0,0 +1,16 @@
+class LinearSearch{
+	public static void main(String args[]){
+	int a[]={1,2,3};
+		int num=3;
+		for(int i=0;i<a.length;i++){
+			if(a[i]==num){
+				
+				System.out.println("Element found at position = "+i);
+			}
+			
+			
+		}
+		
+		
+	}
+}
